a9ba26cbcfc378d8f753281b07fbdc2a0a980f57
JanGospodarek <jan.gospo@onet.pl>
2023-10-15 20:03:24 +0200

    handlig loading

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask
+from flask_restful import Api
+from flask_cors import CORS
+from upload_image import Image
 
+# config
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
 
+# routes
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+api.add_resource(Image,'/upload_image')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
--- frontend/src/components/edit/UploadPhotoStep.vue
+++ frontend/src/components/edit/UploadPhotoStep.vue
@@ -3,19 +3,22 @@
     <div class="flex flex-col items-center rounded-xl shadow-xl p-6">
       <p class="text-5xl font-extrabold p-6">Dodaj nowe zdjęcie</p>
       <div class="flex justify-center">
-        <input type="file" class="file-input w-full max-w-xs" />
-        <button class="btn btn-square btn-success btn-outline">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            fill="currentColor"
-            viewBox="0 0 256 256"
-          >
-            <path
-              d="M227.32,28.68a16,16,0,0,0-15.66-4.08l-.15,0L19.57,82.84a16,16,0,0,0-2.42,29.84l85.62,40.55,40.55,85.62A15.86,15.86,0,0,0,157.74,248q.69,0,1.38-.06a15.88,15.88,0,0,0,14-11.51l58.2-191.94c0-.05,0-.1,0-.15A16,16,0,0,0,227.32,28.68ZM157.83,231.85l-.05.14L118.42,148.9l47.24-47.25a8,8,0,0,0-11.31-11.31L107.1,137.58,24,98.22l.14,0L216,40Z"
-            ></path>
-          </svg>
+        <input
+          type="file"
+          ref="file"
+          name="file"
+          class="file-input w-full max-w-xs"
+          @change="handleFileInputChange()"
+        />
+        <button
+          :class="`btn btn-square btn-success btn-outline  ${
+            (!active || loading) && 'btn-disabled'
+          }`"
+          @click="uploadImage()"
+        >
+          <LoadingSpinner v-if="loading" />
+
+          <SendButton v-if="!loading" />
         </button>
       </div>
       <div class="flex mt-6 w-full justify-between">
@@ -23,17 +26,7 @@
           class="btn btn-accent btn-outline"
           @click="props.handleNextStep('hello')"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            fill="currentColor"
-            viewBox="0 0 256 256"
-          >
-            <path
-              d="M224,128a8,8,0,0,1-8,8H59.31l58.35,58.34a8,8,0,0,1-11.32,11.32l-72-72a8,8,0,0,1,0-11.32l72-72a8,8,0,0,1,11.32,11.32L59.31,120H216A8,8,0,0,1,224,128Z"
-            ></path>
-          </svg>
+          <PrevArrow />
           Wróć
         </button>
         <button
@@ -41,25 +34,47 @@
           @click="props.handleNextStep('upload')"
         >
           Dalej
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            fill="currentColor"
-            viewBox="0 0 256 256"
-          >
-            <path
-              d="M221.66,133.66l-72,72a8,8,0,0,1-11.32-11.32L196.69,136H40a8,8,0,0,1,0-16H196.69L138.34,61.66a8,8,0,0,1,11.32-11.32l72,72A8,8,0,0,1,221.66,133.66Z"
-            ></path>
-          </svg>
+          <NextArrow />
         </button>
       </div>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
-import { defineProps } from "vue";
+import { defineProps, ref } from "vue";
+import convertBase64 from "../../hooks/convertToBase";
+import SendButton from "../../assets/sendButton.vue";
+import LoadingSpinner from "../../assets/loadingSpinner.vue";
+import PrevArrow from "../../assets/prevArrow.vue";
+import NextArrow from "../../assets/nextArrow.vue";
+const file = ref<HTMLInputElement | null>(null);
+const loading = ref<boolean>(false);
+const status = ref<{ status: "ok" | "error" } | null>(null);
+const active = ref<boolean>(false);
 
+const handleFileInputChange = () => {
+  if (file.value?.files && file.value.files[0] !== undefined)
+    active.value = true;
+};
+
+const uploadImage = async () => {
+  if (!file.value?.files || file.value.files[0] == undefined) return;
+  loading.value = true;
+  const base64 = await convertBase64(file.value.files[0]);
+  const res = await fetch("http://127.0.0.1:8000/upload_image", {
+    method: "POST",
+    body: JSON.stringify({ image64: base64 }),
+    headers: { "Content-Type": "application/json" },
+  });
+  const data = await res.json();
+  loading.value = false;
+  if (res.status == 200) {
+    // handle ok
+    console.log(data);
+  } else {
+    // handle error
+  }
+};
 const props = defineProps<{
   handleNextStep: (nextStep: string) => void;
 }>();
